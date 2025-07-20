e460bcf232d9cd87ab8b492173601e6c558f55c9
dokimion24 <leecy3610@gmail.com>
2025-07-21 07:49:08 +0900

    refactor: Redesign Button component to support multiple size variants and full-width option for enhanced flexibility

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,12 +1,39 @@
 import { Button as TamaguiButton, styled } from "tamagui";
 
-export const PrimaryButton = styled(TamaguiButton, {
-  size: "$4",
-  backgroundColor: "$surface",
-  borderColor: "$border",
-  borderWidth: 1,
-  pressStyle: {
-    backgroundColor: "$surfaceHover",
-    borderColor: "$borderHover",
+export const Button = styled(TamaguiButton, {
+  borderRadius: "$2",
+  fontSize: "$2",
+  fontWeight: "$3",
+  height: 36,
+
+  variants: {
+    size: {
+      sm: {
+        height: 28,
+        paddingHorizontal: "$2.5",
+        fontSize: "$1",
+      },
+      md: {
+        height: 36,
+        paddingHorizontal: "$3",
+        fontSize: "$2",
+      },
+      lg: {
+        height: 44,
+        paddingHorizontal: "$4",
+        fontSize: "$3",
+      },
+    },
+
+    fullWidth: {
+      true: {
+        flex: 1,
+        alignSelf: "stretch",
+      },
+    },
+  } as const,
+
+  defaultVariants: {
+    size: "md",
   },
 });
