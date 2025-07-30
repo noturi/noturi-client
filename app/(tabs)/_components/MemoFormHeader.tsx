d874ea9a8bcae0bcd2152ac0127172ec17f30fd5
dokimion24 <leecy3610@gmail.com>
2025-07-30 10:21:47 +0900

    feat: 메모 폼 헤더 컴포넌트 추가

---
+++ app/(tabs)/_components/MemoFormHeader.tsx
@@ -0,0 +1,46 @@
+import { Typography } from "@/components/ui";
+import { Button, XStack, YStack } from "tamagui";
+
+interface MemoFormHeaderProps {
+  onClose: () => void;
+}
+
+export const MemoFormHeader = ({ onClose }: MemoFormHeaderProps) => {
+  return (
+    <>
+      {/* Handle */}
+      <YStack alignItems="center" paddingTop="$2" paddingBottom="$1">
+        <YStack
+          width={36}
+          height={4}
+          backgroundColor="$textMuted"
+          borderRadius="$2"
+        />
+      </YStack>
+
+      {/* Header */}
+      <XStack
+        justifyContent="space-between"
+        alignItems="center"
+        paddingHorizontal="$2"
+        paddingVertical="$3"
+        borderBottomWidth={1}
+        borderBottomColor="$border"
+      >
+        <Button
+          size="$3"
+          backgroundColor="$backgroundTransparent"
+          color="$textSecondary"
+          borderWidth={0}
+          pressStyle={{ backgroundColor: "$backgroundTransparent" }}
+          borderRadius="$4"
+          onPress={onClose}
+        >
+          취소
+        </Button>
+        <Typography variant="heading">새 기록</Typography>
+        <YStack width={60} />
+      </XStack>
+    </>
+  );
+};
