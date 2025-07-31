0aa499b13689916cf7a9b6a2b198dc2f842bb36f
dokimion24 <leecy3610@gmail.com>
2025-07-31 20:38:00 +0900

    refactor: CategoryAddSheet 중복 export 제거 및 코드 정리

--- components/category/index.ts
+++ components/category/index.ts
@@ -1,5 +1,4 @@
+export { CategoryAddSheet } from "./CategoryAddSheet";
 export { CategoryButton } from "./CategoryButton";
 export { CategorySelector } from "./CategorySelector";
-export { CategoryAddSheet } from "./CategoryAddSheet";
 export { SortButton } from "./SortButton";
-export type { Category, SortOption } from "@/lib/category/types";
