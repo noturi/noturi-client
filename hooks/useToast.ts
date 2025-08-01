c647028f8567e81df7bbd657a4eeddab44496ec8
dokimion24 <leecy3610@gmail.com>
2025-08-01 10:22:27 +0900

    feat: 토스트 훅(useToast) 추가 및 성공/오류 메시지 표시 기능 구현

---
+++ hooks/useToast.ts
@@ -0,0 +1,34 @@
+import * as Burnt from "burnt";
+
+export function useToast() {
+  const showSuccess = (message: string) => {
+    Burnt.toast({
+      title: message,
+      preset: "done",
+      layout: {
+        iconSize: {
+          height: 16,
+          width: 16,
+        },
+      },
+    });
+  };
+
+  const showError = (message: string) => {
+    Burnt.toast({
+      title: message,
+      preset: "error",
+      layout: {
+        iconSize: {
+          height: 16,
+          width: 16,
+        },
+      },
+    });
+  };
+
+  return {
+    showSuccess,
+    showError,
+  };
+}
