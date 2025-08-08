54bf19a14772de38aeeec19830f8e1439edd3ed4
dokimion24 <leecy3610@gmail.com>
2025-08-08 10:37:33 +0900

    feat: ESLint 설정 업데이트 및 JSX 속성 정렬 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
-  expoConfig,
+  // expo flat config may export a single config or an array. Normalize to array.
+  ...[].concat(expoConfig),
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
+  },
+  {
+    rules: {
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          ignoreCase: true,
+          reservedFirst: true,
+        },
+      ],
+    },
   },
 ]);
