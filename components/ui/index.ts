0a9a06bad3ac6886905c29431921893c797e9739
dokimion24 <leecy3610@gmail.com>
2025-08-19 19:36:41 +0900

    feat: Form 관련 컴포넌트 추가 - Form, Input, TextArea 컴포넌트를 index.ts에 임포트

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,7 @@
 export * from './ApiErrorBoundary';
 export * from './Button';
+export * from './Form';
+export * from './Input';
 export * from './Loading';
+export * from './TextArea';
 export * from './Typography';
