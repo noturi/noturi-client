49c7550c9700d07208f05c27d66f1558ab19fbe9
dokimion24 <leecy3610@gmail.com>
2025-08-26 16:26:12 +0900

    feat: 메모 목록을 표시하는 MemoSimpleView 컴포넌트 추가

---
+++ src/widgets/memo-list/ui/MemoSimpleView.tsx
@@ -0,0 +1,54 @@
+import { YStack } from 'tamagui';
+import type { UIMemo } from '~/entities/memo/model/types';
+import { Loading, Typography } from '~/shared/ui';
+
+type MemoSimpleViewProps = {
+  memos: UIMemo[];
+  isPending: boolean;
+  isError: boolean;
+  onMemoPress?: (memo: UIMemo) => void;
+};
+
+export function MemoSimpleView({ memos, isPending, isError, onMemoPress }: MemoSimpleViewProps) {
+  if (isPending) {
+    return (
+      <YStack alignItems="center" flex={1} justifyContent="center">
+        <Loading />
+      </YStack>
+    );
+  }
+
+  if (isError) {
+    return (
+      <YStack alignItems="center" flex={1} justifyContent="center">
+        <Typography color="$textMuted">메모를 불러오는데 실패했습니다</Typography>
+      </YStack>
+    );
+  }
+
+  if (memos.length === 0) {
+    return (
+      <YStack alignItems="center" flex={1} justifyContent="center">
+        <Typography color="$textMuted">작성된 메모가 없습니다</Typography>
+      </YStack>
+    );
+  }
+
+  return (
+    <YStack gap="$2" padding="$4">
+      {memos.map((memo) => (
+        <Typography
+          key={memo.id}
+          pressable
+          color="black"
+          paddingVertical="$1"
+          pointerEvents="box-only"
+          size="$3"
+          onPress={() => onMemoPress?.(memo)}
+        >
+          • {memo.title}
+        </Typography>
+      ))}
+    </YStack>
+  );
+}
