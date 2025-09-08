7dd637df45abaabb8d2f3d934602244d8d5daa8c
dokimion24 <leecy3610@gmail.com>
2025-09-08 16:35:16 +0900

    feat: memoFormSchema에서 memoContent 필드 이름을 content로 변경하여 코드 일관성 및 가독성 향상

--- src/entities/memo/model/schemas.ts
+++ src/entities/memo/model/schemas.ts
@@ -6,7 +6,7 @@ export const memoFormSchema = z.object({
     .min(1, '제목을 입력해주세요')
     .max(100, '제목은 100자 이하로 입력해주세요')
     .transform((val) => val.trim()),
-  memoContent: z
+  content: z
     .string()
     .min(1, '내용을 입력해주세요')
     .max(2000, '내용은 2000자 이하로 입력해주세요')
