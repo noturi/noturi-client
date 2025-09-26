23c853f7fb3334515553022c893fdde1adc30d89
dokimion24 <leecy3610@gmail.com>
2025-09-26 14:29:31 +0900

    chore: 메모 리스트 인덱스 파일 수정 - 불필요한 MemoNotificationView 내보내기 제거

--- src/widgets/memo-list/ui/index.ts
+++ src/widgets/memo-list/ui/index.ts
@@ -2,6 +2,5 @@ export { CategoryFilterBar } from './CategoryFilterBar';
 export { MemoListView } from './MemoListView';
 export { MemoRatingGroupView } from './MemoRatingGroupView';
 export { MemoSimpleView } from './MemoSimpleView';
-export { MemoNotificationView } from './MemoNotificationView';
 export { MemoViewToggle, type MemoViewType } from './MemoViewToggle';
 export { SortOptionsBar } from './SortOptionsBar';
