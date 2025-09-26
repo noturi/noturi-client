874bd4ac7d99e53fa8c4d1c85caf891fe6ef315e
dokimion24 <leecy3610@gmail.com>
2025-09-26 14:27:58 +0900

    feat: 통계 쿼리 추가 - 트렌드, 전체 통계 및 카테고리별 통계 조회 기능 구현

---
+++ src/entities/statistics/api/queries.ts
@@ -0,0 +1,27 @@
+import { OverallStatsParamsDto, TrendsParamsDto } from '~/entities/statistics/model/types';
+import { QUERY_KEYS } from '~/shared/lib/tanstack-query/queryKeys';
+
+import { queryOptions } from '@tanstack/react-query';
+
+import { statisticsApi } from './statisticsApi';
+
+// 트렌드 분석 쿼리
+export const trendsQuery = (params: TrendsParamsDto = {}) =>
+  queryOptions({
+    queryKey: QUERY_KEYS.statisticsTrends(params),
+    queryFn: () => statisticsApi.getTrends(params),
+  });
+
+// 전체 통계 쿼리
+export const overallStatsQuery = (params: OverallStatsParamsDto = {}) =>
+  queryOptions({
+    queryKey: QUERY_KEYS.statisticsOverall(params),
+    queryFn: () => statisticsApi.getOverallStats(params),
+  });
+
+// 카테고리별 통계 쿼리
+export const categoryStatsQuery = () =>
+  queryOptions({
+    queryKey: QUERY_KEYS.statisticsCategories,
+    queryFn: () => statisticsApi.getCategoryStats(),
+  });
