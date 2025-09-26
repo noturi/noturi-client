a8ade0a42604d927460095623bc2de8c2f583d17
dokimion24 <leecy3610@gmail.com>
2025-09-26 17:26:45 +0900

    feat: useCalendarDate 훅 추가 - CalendarDateContext에서 날짜 정보를 가져오는 훅 구현

---
+++ src/shared/lib/calendar/useCalendarDate.ts
@@ -0,0 +1,13 @@
+import { use } from 'react';
+
+import { CalendarDateContext, CalendarDateContextValue } from './CalendarDateContext';
+
+export function useCalendarDate(): CalendarDateContextValue {
+  const context = use(CalendarDateContext);
+
+  if (!context) {
+    throw new Error('useCalendarDate는 CalendarDateProvider 내부에서 사용되어야 합니다');
+  }
+
+  return context;
+}
