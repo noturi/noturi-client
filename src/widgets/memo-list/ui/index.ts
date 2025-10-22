63680124892934ac11f9ba8d467de44d58981f27
dokimion24 <leecy3610@gmail.com>
2025-10-22 20:25:37 +0900

    feat: Export RatingView component in memo-list UI index
    - Added export for the new RatingView component in the memo-list UI index file to enhance memo rating functionality.

--- src/widgets/memo-list/ui/index.ts
+++ src/widgets/memo-list/ui/index.ts
@@ -3,4 +3,5 @@ export { MemoListView } from './memo-list-view';
 export { MemoRatingGroupView } from './memo-rating-group-view';
 export { MemoSimpleView } from './memo-simple-view';
 export { MemoViewToggle, type MemoViewType } from './memo-view-toggle';
+export { RatingView } from './rating-view';
 export { SortOptionsBar } from './sort-options-bar';
