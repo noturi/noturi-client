d889faa480d97b4a8b8d313c2c7f67720266dd30
dokimion24 <leecy3610@gmail.com>
2025-10-28 17:53:46 +0900

    feat: RatingMemoCreateScreen for simplicity
    - Removed MemoFormHeader and SafeAreaView to streamline the memo creation interface.
    - Updated background color and layout for improved visual consistency.

--- app/memo/create/index.tsx
+++ app/memo/create/index.tsx
@@ -1,28 +1,16 @@
 import { YStack } from 'tamagui';
 import { MemoFormContent } from '~/features/memo/ui/memo-form-content';
-import { MemoFormHeader } from '~/features/memo/ui/memo-form-header';
-
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { router } from 'expo-router';
 
 export default function RatingMemoCreateScreen() {
-  const handleClose = () => {
-    router.back();
-
-  };
   const handleSuccess = () => {
     router.push('/(tabs)');
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
-      <YStack backgroundColor="$backgroundPrimary" flex={1}>
-        <MemoFormHeader onClose={handleClose} />
-        <YStack flex={1}>
-          <MemoFormContent shouldAutoFocus={true} onSuccess={handleSuccess} />
-        </YStack>
-      </YStack>
-    </SafeAreaView>
+    <YStack backgroundColor="$backgroundSecondary" flex={1}>
+      <MemoFormContent shouldAutoFocus={true} onSuccess={handleSuccess} />
+    </YStack>
   );
 }
