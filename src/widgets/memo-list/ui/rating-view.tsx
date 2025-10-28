cd073198891fa7e892ddffe9ccbd1b5f3e20362b
dokimion24 <leecy3610@gmail.com>
2025-10-28 12:47:01 +0900

    refactor: Simplify RatingView layout and remove FloatingButton
    - Streamlined the RatingView component by removing the FloatingButton and associated logic.
    - Adjusted the layout to enhance readability and maintain a clean structure with updated padding and gap values.

--- src/widgets/memo-list/ui/rating-view.tsx
+++ src/widgets/memo-list/ui/rating-view.tsx
@@ -1,12 +1,7 @@
 import { ScrollView, YStack } from 'tamagui';
 import type { UICategory } from '~/entities/category';
 import type { UIMemo } from '~/entities/memo';
-import { FloatingButton } from '~/shared/ui';
 import { CategoryFilterBar, MemoRatingGroupView } from '~/widgets/memo-list/ui';
-
-import { useCallback } from 'react';
-
-import { router } from 'expo-router';
 
 interface RatingViewProps {
   categories: UICategory[];
@@ -25,26 +20,17 @@ export function RatingView({
   onCategoryPress,
   onMemoPress,
 }: RatingViewProps) {
-  const handleFloatingButtonPress = useCallback(() => {
-    router.push('/memo/create/rating');
-  }, []);
-
   return (
-    <YStack flex={1} position="relative">
-      <ScrollView flex={1} showsVerticalScrollIndicator={false}>
-        <YStack gap="$4" paddingHorizontal="$4" paddingTop="$4">
-          <CategoryFilterBar categories={categories} onPress={onCategoryPress} />
-          <MemoRatingGroupView
-            isError={isError}
-            isPending={isPending}
-            memos={memos}
-            onMemoPress={onMemoPress}
-          />
-        </YStack>
-      </ScrollView>
-      <YStack bottom={140} position="absolute" right="$4">
-        <FloatingButton onPress={handleFloatingButtonPress} />
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <YStack gap="$4">
+        <CategoryFilterBar categories={categories} onPress={onCategoryPress} />
+        <MemoRatingGroupView
+          isError={isError}
+          isPending={isPending}
+          memos={memos}
+          onMemoPress={onMemoPress}
+        />
       </YStack>
-    </YStack>
+    </ScrollView>
   );
 }
