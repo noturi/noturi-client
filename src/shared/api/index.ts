32496b2fb5daf80b3894c12b351873dd337e6dbf
dokimion24 <leecy3610@gmail.com>
2025-12-03 16:45:15 +0900

    feat: enhance API module with new utility function and endpoint exports
    - Exported the ENDPOINTS constant from the endpoints module for improved accessibility.
    - Added a new utility function, toSearchParams, to facilitate the conversion of objects to search parameters, enhancing API request handling.

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,6 +2,8 @@ import ky from 'ky';
 
 import { afterResponseHook, beforeRequestHook } from './hooks';
 
+export { ENDPOINTS } from './endpoints';
+
 // API Instance
 export const api = ky.create({
   prefixUrl: process.env.EXPO_PUBLIC_BASE_URL + '/client',
@@ -16,3 +18,13 @@ export const api = ky.create({
     afterResponse: [afterResponseHook],
   },
 });
+
+export function toSearchParams<T extends object>(
+  params: T,
+): Record<string, string | number | boolean> {
+  return Object.fromEntries(
+    Object.entries(params).filter(
+      ([, value]) => value !== undefined && value !== null && value !== '',
+    ),
+  ) as Record<string, string | number | boolean>;
+}
